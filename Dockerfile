496612e5e80e029777f9170a392e023e1b3d5c0e
Nick Douma <n.douma@nekoconeko.nl>
2016-12-08 11:31:42 +0100

    Initial commit of Dockerfile and all scripts

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3
+MAINTAINER Nick Douma <nick.douma@nekoconeko.nl>
+
+# Install devpi and dependencies
+COPY devpi-requirements.txt /
+RUN pip install -r /devpi-requirements.txt
+
+# Set default server root
+ENV DEVPI_SERVER_ROOT=/devpi
+
+# Add entrypoint
+COPY devpi-client /usr/local/bin/
+COPY entrypoint.sh /
+ENTRYPOINT [ "/bin/bash", "/entrypoint.sh" ]
+EXPOSE 3141
---
+++ entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+function generate_password() {
+    # We disable exit on error because we close the pipe
+    # when we have enough characters, which results in a
+    # non-zero exit status
+    set +e
+    tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1 | tr -cd '[:alnum:]'
+    set -e
+}
+
+
+function kill_devpi() {
+    devpi-server --stop
+}
+
+if [ "${1:-}" == "bash" ]; then
+    exec "$@"
+fi
+
+DEVPI_ROOT_PASSWORD="${DEVPI_ROOT_PASSWORD:-}"
+if [ -f "$DEVPI_SERVER_ROOT/.root_password" ]; then
+    DEVPI_ROOT_PASSWORD=$(cat "$DEVPI_SERVER_ROOT/.root_password")
+elif [ -z "$DEVPI_ROOT_PASSWORD" ]; then
+    DEVPI_ROOT_PASSWORD=$(generate_password)
+fi
+
+initialize=no
+if [ ! -f "$DEVPI_SERVER_ROOT/.serverversion" ]; then
+    initialize=yes
+fi
+
+if [ ! -d "$DEVPI_SERVER_ROOT" ]; then
+    echo "ENTRYPOINT: Creating devpi-server root"
+    mkdir -p "$DEVPI_SERVER_ROOT"
+fi
+
+echo "ENTRYPOINT: Starting devpi-server"
+devpi-server --start --host 0.0.0.0 --port 3141 --serverdir "$DEVPI_SERVER_ROOT" --theme semantic-ui "$@"
+
+echo "ENTRYPOINT: Installing signal traps"
+trap kill_devpi SIGINT SIGTERM
+
+if [ "$initialize" == "yes" ]; then
+    echo "ENTRYPOINT: Initializing devpi-server"
+    devpi use http://localhost:3141
+    devpi login root --password=''
+    echo "ENTRYPOINT: Setting root password"
+    devpi user -m root "password=$DEVPI_ROOT_PASSWORD"
+    echo -n "$DEVPI_ROOT_PASSWORD" > "$DEVPI_SERVER_ROOT/.root_password"
+    devpi logoff
+fi
+
+echo "ENTRYPOINT: Tailing log"
+tail -f "$DEVPI_SERVER_ROOT/.xproc/devpi-server/xprocess.log"
